56f12cd79c5a78c3bab08f6a74f5d4332b25d169
andreforchesatto@gmail.com <andreforchesatto@gmail.com@74253452-7420-a7ef-af24-dfbb770cb2d3>
2012-05-11 20:14:10 +0000

    Primeiro commit

---
+++ projetoMaven/src/main/java/br/unoesc/cursoComputacao/model/Pessoa.java
@@ -0,0 +1,20 @@
+package br.unoesc.cursoComputacao.model;
+
+public class Pessoa {
+
+	private String nome;
+
+	public Pessoa(String nome) {
+		super();
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
